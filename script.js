02141edf50561d763f22ba5323041de662453150
sabrina <sabrinaabrodi20@gmail.com>
2022-06-23 20:05:11 +0200

    selecting required elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
